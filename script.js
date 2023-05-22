2391486b6c9797ba985b1adf291d98c90de06f49
Artur56-Dev <131680937+Artur56-Dev@users.noreply.github.com>
2023-05-22 22:07:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let bodyWidth = document.body.clientWidth;
+console.log(bodyWidth);
+const div = document.querySelector('.headerdiv');
+const placeholder = document.createElement('div');
+placeholder.style.height = div.offsetHeight + 'px'; 
+placeholder.style.display = 'none'; 
+div.parentNode.insertBefore(placeholder, div);
+const threshold = div.offsetTop;
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > threshold) {
+      div.classList.add('fixed-top');
+      placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
+    } else {
+      div.classList.remove('fixed-top');
+      placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
+    }
+  });
