d04b9a44529536b6ce3c3a39bff85a14899d3cbc
Artur56-Dev <131680937+Artur56-Dev@users.noreply.github.com>
2023-05-26 02:41:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+
 let bodyWidth = document.body.clientWidth;
-let bodyHeight = document.body.clientHeight;
 console.log(bodyWidth);
-console.log(bodyHeight);
+window.addEventListener('load', function () {
+  const hellodiv = document.querySelector('.maindiv .hellodiv');
+  const conteiner1 = document.querySelector('.maincontent .conteiner1');
+  console.log('Высоты div:' + hellodiv.offsetHeight);
+  if (hellodiv.offsetHeight > window.innerHeight) {
+    hellodiv.style.height = "100vh";
+    console.log('div больше и должен обрезатся');
+  }
+  if(this.window.innerWidth > this.window.innerHeight){
+    conteiner1.style.height = "80vh";
+    console.log("Ширина "+this.window.innerWidth + " больше " + "Высоты " + this.window.innerHeight)
+  }
+  else{
+    conteiner1.style.height = "80vw";
+    console.log("Ширина "+ this.window.innerWidth + " меньше " + "Высоты " + this.window.innerHeight)
+  }
+});
+console.log('Видимоя высота:' + window.innerHeight);
+
 const div = document.querySelector('.headerdiv');
 const placeholder = document.createElement('div');
-placeholder.style.height = div.offsetHeight + 'px'; 
-placeholder.style.display = 'none'; 
+placeholder.style.height = div.offsetHeight + 'px';
+placeholder.style.display = 'none';
 div.parentNode.insertBefore(placeholder, div);
 const threshold = div.offsetTop;
 window.addEventListener('scroll', () => {
-    if (window.pageYOffset > threshold) {
-      div.classList.add('fixed-top');
-      placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
-    } else {
-      div.classList.remove('fixed-top');
-      placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
-    }
-  });
+  if (window.pageYOffset > threshold) {
+    div.classList.add('fixed-top');
+    placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
+  } else {
+    div.classList.remove('fixed-top');
+    placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
+  }
+});
+
+
+
